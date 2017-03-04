5db6ed138a56a89cea184f14359dbfaf6364d6fc
mvavruska <mattvavruska@gmail.com>
2017-03-03 23:56:32 -0500

    g

--- main.cpp
+++ main.cpp
@@ -8,13 +8,23 @@ struct Location {
 	int coords[3];
 	string description;
 	Location(int[3] coordinates, string desc) {
-
+		coords = coordinates;
+		description = desc;
 	}
+};
+
+class Player {
+private:
+	bool beatTower;
+	bool beatGame;
+	bool fled;
+	bool died;
 };
 
 int main()
 {
-	//vector of locations visited
+	vector<Location> visitedlocations;//vector of locations visited
+	int code = rand() % 9000 + 1000;
 	//start location set to [0,0,0]
 	return 0;
 }
